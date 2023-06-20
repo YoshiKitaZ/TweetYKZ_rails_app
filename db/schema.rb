688d030357cf8d925400044ab1623a63e5f470d0
YoshiKitaZ <130706787+YoshiKitaZ@users.noreply.github.com>
2023-06-20 15:29:53 +0900

    commit number3

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_17_221804) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_20_062701) do
+  create_table "likes", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "posts", force: :cascade do |t|
     t.text "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
